d462cdafeb0fabe30f1bb9eded3ba23cb0e7c00a
Faizanmehdi-pro <faizanmehdi69@gmail.com>
2025-06-14 14:45:37 +0500

    first commit

---
+++ email_app_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmailAppManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'email_app_management'
---
+++ email_app_management/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class CustomerDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.email}"
+
+
